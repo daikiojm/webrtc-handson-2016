2b961a579cfa75012a0f8b6297fbf675d8d4dab8
d_ojima <daiking.ca2@gmail.com>
2017-04-09 14:27:14 +0900

    WebRTCのAPIを利用するための準備処理を追加する

--- client/webrtc.js
+++ client/webrtc.js
@@ -7,12 +7,12 @@ let localStream = null;
 let peerConnection = null;
 
 // getUserMediaでカメラ、マイクにアクセス
-function startVideo() {
+let startVideo = () => {
     navigator.mediaDevices.getUserMedia({video: { frameRate: { min: 10, max: 15 } }, audio: true})
-        .then(function (stream) { // success
+        .then((stream) => { // success
             playVideo(localVideo,stream);
             localStream = stream;
-        }).catch(function (error) { // error
+        }).catch((error) => { // error
         console.error('mediaDevice.getUserMedia() error:', error);
         return;
     });
@@ -21,4 +21,55 @@ function startVideo() {
 let playVideo = (element, stream) => {
     element.srcObject = stream;
     element.play();
+}
+
+// WebRTCを利用する準備をする
+let prepareNewConnection = () => {
+    // RTCPeerConnectionを初期化する
+    // ここではskywayのstunサーバーを指定している
+    const pc_config = {"iceServers":[ {"urls":"stun:stun.skyway.io:3478"} ]};
+    const peer = new RTCPeerConnection(pc_config);
+
+    // リモートのストリームを受信した場合のイベントをセット
+    if ('ontrack' in peer) {
+        peer.ontrack = function(event) {
+            console.log('-- peer.ontrack()');
+            playVideo(remoteVideo, event.streams[0]);
+        };
+    }
+    else {
+        peer.onaddstream = function(event) {
+            console.log('-- peer.onaddstream()');
+            playVideo(remoteVideo, event.stream);
+        };
+    }
+
+    // ICE Candidateを収集したときのイベント
+    peer.onicecandidate = function (evt) {
+        if (evt.candidate) {
+            console.log(evt.candidate);
+        } else {
+            console.log('empty ice event');
+            sendSdp(peer.localDescription);
+        }
+    };
+
+    // ローカルのストリームを利用できるように準備する
+    if (localStream) {
+        console.log('Adding local stream...');
+        peer.addStream(localStream);
+    }
+    else {
+        console.warn('no local stream, but continue.');
+    }
+
+    return peer;
+}
+
+// 手動シグナリングのための処理を追加する
+let sendSdp => (sessionDescription) {
+    console.log('---sending sdp ---');
+    textForSendSdp.value = sessionDescription.sdp;
+    textForSendSdp.focus();
+    textForSendSdp.select();
 }
