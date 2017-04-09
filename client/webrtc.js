8827a7d2fd98e309e1a2d9e41854131d2df4e780
d_ojima <daiking.ca2@gmail.com>
2017-04-09 14:14:23 +0900

    getUserMediaに必要な処理を追加する

--- client/webrtc.js
+++ client/webrtc.js
@@ -1,6 +1,24 @@
+'use strict';
 const localVideo = document.getElementById('local_video');
 const remoteVideo = document.getElementById('remote_video');
 const textForSendSdp = document.getElementById('text_for_send_sdp');
 const textToReceiveSdp = document.getElementById('text_for_receive_sdp');
 let localStream = null;
 let peerConnection = null;
+
+// getUserMediaでカメラ、マイクにアクセス
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+        .then(function (stream) { // success
+            playVideo(localVideo,stream);
+            localStream = stream;
+        }).catch(function (error) { // error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
+}
+// Videoの再生を開始する
+let playVideo = (element, stream) => {
+    element.srcObject = stream;
+    element.play();
+}
